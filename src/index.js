09ea5635d67ff57ba37862026172b49cfaaed4e5
Andrew McRobb <andrewmcrobb@gmail.com>
2018-09-09 23:32:06 -0700

    Added travis and linting support.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import 'promise-polyfill';
-// import 'isomorphic-fetch';
 import { h, render } from 'preact';
+
+// eslint-disable-next-line import/no-unresolved
 import './style';
 
 let root;
 function init() {
-	let App = require('./components/app').default;
-	root = render(<App />, document.body, root);
+  // eslint-disable-next-line global-require
+  const App = require('./components/app').default;
+  // eslint-disable-next-line react/jsx-filename-extension
+  root = render(<App />, document.body, root);
 }
 
 // in development, set up HMR:
 if (module.hot) {
-	require('preact/devtools');   // turn this on if you want to enable React DevTools!
-	module.hot.accept('./components/app', () => requestAnimationFrame(init) );
+  // eslint-disable-next-line global-require
+  require('preact/devtools'); // turn this on if you want to enable React DevTools!
+  module.hot.accept('./components/app', () => requestAnimationFrame(init));
 }
 
 init();
-
