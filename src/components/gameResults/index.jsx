06318148e7ce9dbb182b587f47bd69abfc195f37
Andrew McRobb <andrewmcrobb@gmail.com>
2018-11-10 23:09:29 -0700

    [wip] Fixed packages and styling.

--- src/components/gameResults/index.jsx
+++ src/components/gameResults/index.jsx
@@ -2,25 +2,25 @@ import { h, Component } from 'preact';
 
 import './style';
 
+import DebianSvg from 'steamplaydb-distro-icons/src/debian.svg';
+
 const GameResult = () => {
 
   const className = `gameResult column`;
 
   return (
-    <div className={className}>
+    <a className={className}>
       <div className="hero is-dark">
         <div className="hero-body is-paddingless">
           <div className="columns">
             <div className="column is-one-quarter">
               <figure className="image is-2by1">
-                <a>
                   <img src="https://steamcdn-a.akamaihd.net/steam/apps/379720/header.jpg" alt=""/>
-                </a>
               </figure>
             </div>
               <div class="column content">
-                <a><h2 className="title">DOOM (2016)</h2></a>
-                <div class="columns">
+                <h2 className="title">DOOM (2016)</h2>
+                <div class="columns is-multilined">
                   <div class="column">
                     <span class="tag is-black is-medium"><i className="la la-check"/> Playability: <span class="has-text-success">9.0</span></span>
                   </div>
@@ -35,7 +35,7 @@ const GameResult = () => {
           </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
